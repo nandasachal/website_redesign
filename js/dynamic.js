4362d8cdd0914dbff9e6586a23b2b1e3c83c3197
Sachal Nanda <nandasachal@gmail.com>
2016-06-08 22:33:14 -0700

    got rid of comments

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-	// $("div.not_clicked").click(function(){
-	// 	$("div.clicked").toggleClass(".clicked", ".not_clicked");
-	// 	$(this).toggleClass(".not_clicked", ".clicked");
-	// })
-	// $("#second_tab").click(function(){
-	// 	$("main").load("../test.html");
-	// });
-
 	function tab_switch(tab, view){
 		$(".clicked").addClass("not_clicked");
 		$(".clicked").removeClass("clicked");
@@ -24,18 +16,6 @@ $(document).ready(function(){
 	$("#involvement").hide();
 	$("#projects").hide();
 
-	// $("#second_tab").click(function(){
-	// 	$(".clicked").addClass("not_clicked");
-	// 	$(".clicked").removeClass("clicked");
-
-	// 	$("#second_tab").removeClass("not_clicked");
-	// 	$("#second_tab").addClass("clicked");
-
-	// 	$("#work_experience").hide();
-	// 	$("#projects").hide();
-	// 	$("#involvement").fadeIn(400); //400milliseconds
-	// });
-
 	$("#first_tab").click(function(){
 		tab_switch("#first_tab", "#work_experience");
 	});
@@ -48,36 +28,4 @@ $(document).ready(function(){
 		tab_switch("#third_tab", "#projects");
 	});
 
-	// $("#third_tab").click(function(){
-	// 	$(".clicked").addClass("not_clicked");
-	// 	$(".clicked").removeClass("clicked");
-
-	// 	$("#third_tab").removeClass("not_clicked");
-	// 	$("#third_tab").addClass("clicked");
-
-	// 	$("#work_experience").hide();
-	// 	$("#involvement").hide();
-	// 	$("#projects").fadeIn(400); //400milliseconds
-	// });
-
-	// $("#first_tab").click(function(){
-	// 	$(".clicked").addClass("not_clicked");
-	// 	$(".clicked").removeClass("clicked");
-
-	// 	$("#first_tab").removeClass("not_clicked");
-	// 	$("#first_tab").addClass("clicked");
-
-	// 	$("#involvement").hide();
-	// 	$("#projects").hide();
-	// 	$("#work_experience").fadeIn(400); //400milliseconds
-	// });
-
-
-	// $("#second_tab").click(function(){
-	// 	$('#work_experience').fadeOut("slow", function(){
-	// 	    $('#work_experience').load("../test.html", function(){
-	// 	        $('#work_experience').fadeIn("slow");
-	// 	    });
-	// 	});
-	// });
 });
